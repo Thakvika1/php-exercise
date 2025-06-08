7805bdffa164b641c3bfe37f346961f5d2b6cb96
Thakvika1 <thakvika.thorn@gmail.com>
2025-06-08 22:22:19 +0700

    exercise

--- exercise.php
+++ exercise.php
@@ -26,4 +26,33 @@ function number($num){
   }
 }
 
-echo number(20);
+echo number(20). "<br />";
+
+// function sum total number
+function sum($num){
+    $sum = 0;
+
+    for( $i = 0; $i <= $num; $i++){
+        $sum += $i;
+    }
+    echo $sum;
+}
+
+echo sum(30). "<br />";
+
+
+// function factorial
+function factorial($num){
+    $totalFactorial = 1;
+    if($num == 1 || $num == 0){
+        echo $totalFactorial;
+    }
+    else{
+        for($i = 1; $i <= $num; $i++){
+            $totalFactorial *= $i;
+        }
+        echo $totalFactorial;
+    }
+}
+
+echo factorial(5);
