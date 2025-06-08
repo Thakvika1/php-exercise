b94c0be06a9ec97a2564eced15f47c10c3c076a4
Thakvika1 <thakvika.thorn@gmail.com>
2025-06-08 22:27:23 +0700

    exercise

--- exercise.php
+++ exercise.php
@@ -28,6 +28,7 @@ function number($num){
 
 echo number(20). "<br />";
 
+
 // function sum total number
 function sum($num){
     $sum = 0;
@@ -55,4 +56,39 @@ function factorial($num){
     }
 }
 
-echo factorial(5);
+echo factorial(5). "<br />";
+
+
+// function calculate your score
+function grade($score){
+
+    if ($score >= 90 && $score <= 100) {
+        echo 'Your grade is A+';
+    }
+
+    else if ($score >= 80 && $score <= 89) {
+        echo 'Your grade is A';
+    }
+
+    else if ($score >= 70 && $score <= 79) {
+        echo 'Your grade is B';
+    }
+
+    else if ($score >= 60 && $score <= 69) {
+        echo 'Your grade is C';
+    }
+
+    else if ($score >= 50 && $score <= 59) {
+        echo 'Your grade is D';
+    }
+
+    else if ($score >= 0 && $score <= 49) {
+        echo 'Your grade is F';
+    }
+
+    else {
+        echo 'Your score is Error';
+    }
+}
+
+echo grade(100);
